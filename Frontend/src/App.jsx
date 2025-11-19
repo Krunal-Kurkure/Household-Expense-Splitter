57147055b252ca6faed6850ab54a8148fdfc02b4
Krunal Kurkure <adkurkure@gmail.com>
2025-11-19 18:08:35 +0530

    Basic setup of project is done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-700">
+      <h1 className="text-white text-2xl font-bold">Can We Start To Code</h1>
+    </div>
+  );
+};
+
+export default App;
